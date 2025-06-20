b99fb9701da0358da185de062f0bec1c60ec759a
ChandraAdhikarla <chandra.adhikarla@samespace.com>
2025-06-20 13:06:47 +0000

    add

--- src/google/adk/tools/transfer_to_agent_tool.py
+++ src/google/adk/tools/transfer_to_agent_tool.py
@@ -14,12 +14,6 @@
 
 from .tool_context import ToolContext
 
-<<<<<<< HEAD
-# TODO: make this internal, since user doesn't need to use this tool directly.
-def transfer_to_agent(agent_name: str, tool_context: ToolContext):
-  """Transfer the question to another agent."""
-  # logger.info(f"Transfer to agent: {agent_name}, tool_context: {tool_context._invocation_context}")
-=======
 
 def transfer_to_agent(agent_name: str, tool_context: ToolContext):
   """Transfer the question to another agent.
@@ -30,5 +24,4 @@ def transfer_to_agent(agent_name: str, tool_context: ToolContext):
   Args:
     agent_name: the agent name to transfer to.
   """
->>>>>>> main
   tool_context.actions.transfer_to_agent = agent_name
